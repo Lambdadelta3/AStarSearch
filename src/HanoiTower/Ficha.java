040b7646c5efaddfaafd6634f1da1861001e1e61
Yoitsu36 <34716366+Yoitsu36@users.noreply.github.com>
2018-12-06 19:28:27 +0100

    Create Ficha.java

---
+++ src/HanoiTower/Ficha.java
@@ -0,0 +1,20 @@
+/*
+ * Basic disk structure
+ */
+package HanoiTower;
+
+
+public class Ficha {
+    
+    int valor;
+    
+    Ficha (int v)
+    {
+        this.valor = v;
+    }
+    
+    public int getValor()
+    {
+        return this.valor;
+    }
+}
