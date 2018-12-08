27d74f4005dc86465d9f7a78fd230e100408a2d2
Yoitsu36 <34716366+Yoitsu36@users.noreply.github.com>
2018-12-08 05:44:28 +0100

    Create NPuzzle.java

---
+++ src/NPuzzle/NPuzzle.java
@@ -0,0 +1,170 @@
+/*
+ *Node implementation for the 8 Puzzle and 15 Puzzle games
+ */
+package NPuzzle;
+
+import AStarSearch.NodoAbstracto;
+import AStarSearch.NodoInterfaz;
+import static java.lang.Math.abs;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class NPuzzle extends NodoAbstracto{
+    
+    int [][] estado;
+    //coordinates of the blank or 0
+    int x;
+    int y;
+    int precision;
+    
+    public NPuzzle(int [][] estadoIni, int pre)
+    {
+        super();
+        this.estado = estadoIni.clone();
+        for(int i=0; i<this.estado.length; i++)
+        {
+            for(int j=0; j<this.estado[i].length; j++)
+            {
+                if(this.estado[i][j]==0)
+                {
+                    this.x = i;
+                    this.y = j;
+                }
+            }
+        }
+        this.precision=pre;
+        
+    }
+    
+    public NPuzzle(NPuzzle p)
+    {
+        super(p);
+        this.estado = p.estado.clone();
+        for (int i = 0; i < this.estado.length; i++) 
+        {
+            this.estado[i] = p.estado[i].clone();
+        }
+        this.x = p.x;
+        this.y = p.y;
+        this.precision=p.precision;
+    }
+    
+
+    @Override
+    public boolean inseguro() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean esIgual(NodoInterfaz b) {
+        if(Arrays.deepEquals(this.estado, ((NPuzzle)b).estado))
+                return true;
+        
+        return false;
+    }
+
+    @Override
+    public void escribirNodo() {
+        System.out.println("");
+        System.out.println("Nodo Valor " + this.getCalidad() + ":");
+        for(int[] a : this.estado)
+        {
+            System.out.println("");
+            System.out.print("|");
+            for(int i : a)
+            {
+                System.out.print(i + "|");
+            }
+            System.out.println("");
+            for(int i : a)
+            {
+                System.out.print("_");
+                System.out.print("_");
+            }
+        }
+    }
+
+    @Override
+    public List<NodoInterfaz> expandir() {
+         List<NodoInterfaz> nodos = new ArrayList();
+        
+         NPuzzle nodoNP;
+        //Izquierda
+        if(this.y>0)
+        {
+            nodoNP = new NPuzzle(this);
+            nodoNP.estado[x][y] = nodoNP.estado[x][y-1];
+            nodoNP.estado[x][y-1]= 0;
+            nodoNP.y = nodoNP.y-1;
+            nodos.add(nodoNP);
+        }
+        
+        //Derecha
+        if(this.y+1 < this.estado[x].length)
+        {
+            nodoNP = new NPuzzle(this);
+            nodoNP.estado[x][y] = nodoNP.estado[x][y+1];
+            nodoNP.estado[x][y+1]= 0;
+            nodoNP.y = nodoNP.y+1;
+            nodos.add(nodoNP);
+        }
+        
+        //Arriba
+        if(this.x>0)
+        {
+            nodoNP = new NPuzzle(this);
+            nodoNP.estado[x][y] = nodoNP.estado[x-1][y];
+            nodoNP.estado[x-1][y]= 0;
+            nodoNP.x = nodoNP.x-1; 
+            nodos.add(nodoNP);
+        }
+        
+        //Abajo
+        if(this.x+1 < this.estado.length)
+        {
+            nodoNP = new NPuzzle(this);
+            nodoNP.estado[x][y] = nodoNP.estado[x+1][y];
+            nodoNP.estado[x+1][y]= 0;
+            nodoNP.x = nodoNP.x+1; 
+            nodos.add(nodoNP);
+        }
+        
+        
+        
+        return nodos;
+    }
+
+    @Override
+    //Evaluation is given by the amount of pices outside of their position
+    public void evaluar() {
+        
+        int descolocados = 0;
+        int row;
+        int col;
+        
+        for(int i=0; i<this.estado.length; i++)
+        {
+            for(int j=0; j<this.estado[i].length; j++)
+            {
+                //counts dispositioned pieces
+                if(this.estado[i][j] != 0)
+                {
+                row = (this.estado[i][j]-1)/this.estado[i].length;
+                col = (this.estado[i][j]-1)%this.estado[i].length;
+                
+                row = abs(row-i);
+                col = abs(col-j);
+                
+                descolocados = descolocados + (row + col)*precision;
+                }
+            }
+        }
+        
+        this.setEvaluacion(descolocados);
+        this.setCalidad(this.getEvaluacion() + this.getProfundidad());
+        
+    }
+    
+}
