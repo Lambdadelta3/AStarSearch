bceed97f825a90ea15c2f44e26c675331804e133
Yoitsu36 <34716366+Yoitsu36@users.noreply.github.com>
2018-12-08 05:39:01 +0100

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,13 +6,28 @@ package Main;
 import AStarSearch.AStarSearch;
 import AStarSearch.NodoInterfaz;
 import HanoiTower.HanoiTower;
+import NPuzzle.NPuzzle;
 
 
-public class Main 
-{
+public class Main {
+
+    // Hanoi towers, arguments are just 3 arrays with each column
+    static NodoInterfaz origenHanoi = (NodoInterfaz) new HanoiTower( new int[] {1,2,3,4,5,6,7,8,9,10},new int[]  {},new int[]  {});
+    static NodoInterfaz destinoHanoi = (NodoInterfaz) new HanoiTower( new int[] {},new int[]  {},new int[]  {1,2,3,4,5,6,7,8,9,10});
     
-    static NodoInterfaz origen = (NodoInterfaz) new HanoiTower( new int[] {1,2,3,4,5,6,7,8,9,10},new int[]  {},new int[]  {});
-    static NodoInterfaz destino = (NodoInterfaz) new HanoiTower( new int[] {},new int[]  {},new int[]  {1,2,3,4,5,6,7,8,9,10});
+    
+    //N-puzzle, arguments are 1- a matrix of each value of a state 2- the precision of heuristics, 
+    //high values can be faster, but will be less precise, for 8-puzzle 1 is fine, for 15-puzzle or
+    //bigger 3 or more is adviced
+    
+    // 8-Puzzle
+    static NodoInterfaz origen8Puzzle = (NodoInterfaz) new NPuzzle( new int[][] {{3,0,7},{2,8,1},{6,4,5}},1);
+    static NodoInterfaz destino8Puzzle = (NodoInterfaz) new NPuzzle( new int[][] {{1,2,3},{4,5,6},{7,8,0}},1);
+    
+    
+    // 15-Puzzle
+    static NodoInterfaz origen15Puzzle = (NodoInterfaz) new NPuzzle( new int[][] {{15,2,1,12},{8,5,6,11},{4,9,10,7}, {3,0,13,14}},3);
+    static NodoInterfaz destino15Puzzle = (NodoInterfaz) new NPuzzle( new int[][] {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}},3);
     
     
     
@@ -23,7 +38,7 @@ public class Main
         // TODO code application logic here
 
         
-        AStarSearch res = new AStarSearch(origen, destino);
+        AStarSearch res = new AStarSearch(origen8Puzzle, destino8Puzzle);
         
         
     }
